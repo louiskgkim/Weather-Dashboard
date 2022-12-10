fc1b0d17457252bee284e6c36c1a17b648879b04
louiskgkim <lkgk31@gmail.com>
2022-12-09 22:27:40 -0800

    added general start of js file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let key = ''
+let city = 'Hawaii'
+
+// Current Time and Date
+let date = dayjs().format('dddd, MMMM DD YYYY');
+let dateTime = dayjs().format('dddd, MMMM DD YYYY, hh:mm.ss');
+
+let cityList = [];
+
